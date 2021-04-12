ded9aa8c00cc2197a06b9103e2dbfe4c7418c0fe
Gogo-Dp <81482277+Gogo-Dp@users.noreply.github.com>
2021-04-12 15:17:15 +0200

    Update host.js

--- public/host.js
+++ public/host.js
@@ -16,7 +16,7 @@ Run http-server -c-1 -p80 to start server on open port 80.
 // const serverIp      = 'https://yourservername.herokuapp.com';
 // const serverIp      = 'https://yourprojectname.glitch.me';
 const serverIp      = '127.0.0.1';
-const serverPort    = '3000';
+const serverPort    = '80';
 const local         = true;   // true if running locally, false
                               // if running on remote server
 
